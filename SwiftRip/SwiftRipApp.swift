bed9b912917ac36d20870baf12097db80c0c74d5
Ryan Fahlsing <ryan@fahlsing.com>
2026-05-16 21:24:17 -0400

    Initial Commit

---
+++ SwiftRip/SwiftRipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRipApp.swift
+//  SwiftRip
+//
+//  Created by Ryan Fahlsing on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftRipTests/SwiftRipTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftRipTests.swift
+//  SwiftRipTests
+//
+//  Created by Ryan Fahlsing on 5/16/26.
+//
+
+import Testing
+@testable import SwiftRip
+
+struct SwiftRipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
